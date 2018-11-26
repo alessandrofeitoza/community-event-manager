83fece902abec18479806be2aa48883dcfc536cc
alessandrofeitoza <eu@alessandrofeitoza.eu>
2018-11-26 10:40:56 -0300

    Managing slots to space

--- src/Repository/Schedule/ScheduleRepositoryInterface.php
+++ src/Repository/Schedule/ScheduleRepositoryInterface.php
@@ -27,6 +27,11 @@ interface ScheduleRepositoryInterface
      */
     public function findBy(array $criteria = [], array $orderBy = [], int $limit = null, int $offset = null);
 
+    /**
+     * @return Schedule[]
+     */
+    public function findBy(array $criteria = [], array $orderBy = [], int $limit = null, int $offset = null);
+
     public function createFrom(ScheduleRequest $scheduleRequest): Schedule;
 
     public function nextIdentity(): UuidInterface;
